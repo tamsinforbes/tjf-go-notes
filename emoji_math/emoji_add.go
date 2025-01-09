5cfa1accf87ee8fa95e595f3c9bca4061b9a5005
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2025-01-09 16:07:49 +0000

    initiate emoji_math package

---
+++ emoji_math/emoji_add.go
@@ -0,0 +1,6 @@
+package emoji_add
+
+func add_emoji(x, y rune) (z rune) {
+	z = x + y
+	return
+}
--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/tamsinforbes/tjf-go-notes/emoji_math"
 )
 
 func main() {
 
 	fmt.Println("Yo!")
+	
 
 }
